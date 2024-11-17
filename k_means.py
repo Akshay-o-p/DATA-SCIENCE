0c1180ea781c6eb24b2944b29ca34edf9840cdf0
Akshay O P <159622996+Akshay-o-p@users.noreply.github.com>
2024-11-17 13:55:23 +0530

    Update k_means.py

--- k_means.py
+++ k_means.py
@@ -18,3 +18,10 @@ print("\n")
 centroid=classifier.cluster_centers_
 print(centroid)
 
+plt.scatter(x.iloc[:, 0], x.iloc[:, 1], c=y, cmap='viridis', s=50)
+plt.scatter(centroid[:, 0], centroid[:, 1], c='red', marker='X', s=200, label='Centroids')
+plt.title("KMeans Clustering on Iris Data")
+plt.xlabel("Sepal Length")
+plt.ylabel("Sepal Width")
+plt.legend()
+plt.show()
